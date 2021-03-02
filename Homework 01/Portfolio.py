5abaf41744d32d96b72f8b8656efe12888df4974
Abdul <manofsteel272@yahoo.com>
2021-03-03 00:28:39 +0300

    Add sellStock and sellMutualFund. Add tests to main class

--- Homework 01/Portfolio.py
+++ Homework 01/Portfolio.py
@@ -4,7 +4,10 @@ class Portfolio():
         self.cash = 0.0
 
         self.stocks = {} 
-        self.mutual_funds = []
+        self.mutual_funds = {}
+
+        self.stocks_prices = {}
+        self.mutual_funds_prices = {}
 
         self.transaction_history = []
         self.transaction_number = 0
@@ -12,13 +15,13 @@ class Portfolio():
 
     def addCash(self, cash_amount):
         self.cash += cash_amount
-        print("${} added successfully. Current balance: {}".format(cash_amount, self.cash))
+        print("${} added successfully. Current balance: {}".format(cash_amount, "{:.2f}".format(self.cash)))
         self.addTransaction("Cash added", cash_amount)
 
     def withdrawCash(self, amount):                                 
         if amount <= self.cash:
             self.cash -= amount 
-            print("${} withdrawn successfully. Current balance: {}".format(amount, self.cash))
+            print("${} withdrawn successfully. Current balance: {}".format(amount, "{:.2f}".format(self.cash)))
             self.addTransaction("Cash withdrawn", - amount)
         else :
             print("Cash withdrawal failed: not enough cash")
@@ -31,6 +34,8 @@ class Portfolio():
         if self.cash >= amount_to_pay:
             self.cash -= amount_to_pay
 
+            self.stocks_prices[stock.symbol] = stock.price
+
             if stock.symbol in self.stocks.keys():
                 self.stocks[stock.symbol] = self.stocks[stock.symbol] + amount
             else :
@@ -42,15 +47,21 @@ class Portfolio():
         else : 
             print("Transaction failed: not enough cash")
 
-    def buyMutualFund(self, share, mutual_fund):
-        amount_to_pay = 1 / share
+    def buyMutualFund(self, shares, mutual_fund):
+        amount_to_pay = 1 / shares
 
         if self.cash >= amount_to_pay:
             self.cash -= amount_to_pay
-            self.mutual_funds.append(mutual_fund)
 
-            print("Mutual fund/s {} successfully purchased!".format(mutual_fund))
-            self.addTransaction("Mutual fund {} bought".format(mutual_fund), amount_to_pay)
+            self.mutual_funds_prices[mutual_fund.symbol] = mutual_fund.price
+
+            if mutual_fund.symbol in self.mutual_funds.keys():
+                self.mutual_funds[mutual_fund.symbol] = self.mutual_funds[mutual_fund.symbol] + shares
+            else :
+                self.mutual_funds[mutual_fund.symbol] = shares
+
+            print("Mutual fund/s successfully purchased!")
+            self.addTransaction("{} {} Mutual funds bought".format(shares, mutual_fund.symbol), amount_to_pay)
         else : 
             print("Transaction failed: not enough cash")
 
@@ -60,22 +71,36 @@ class Portfolio():
         else:
             if symbol in self.stocks.keys():
                 self.stocks[symbol] = self.stocks[symbol] - amount
-                self.cash += 1
+                self.cash += self.stocks_prices[symbol]
             else :
                 print("No such stock available")
+    
+    def sellMutualFund(self, symbol, shares):
+        if shares > self.mutual_funds[symbol]:
+            print("No enough shares to be sold!")
+        else:
+            if symbol in self.mutual_funds.keys():
+                self.mutual_funds[symbol] = self.mutual_funds[symbol] - shares
+                self.cash += self.mutual_funds_prices[symbol]
+            else :
+                print("No such mutual funds available")    
                 
 
     def addTransaction(self, message, amount):
-        self.transaction_history.append((self.transaction_number, "{} for ${}".format(message, "{:.2f}".format(amount))))
+        self.transaction_history.append((self.transaction_number, "{} | ${}".format(message, "{:.2f}".format(abs(amount)))))
         self.transaction_number += 1
 
     def history(self):
         transaction_hist = sorted(self.transaction_history, key=lambda x : x[0])
+
+        print("\n----Transaction History----\n")
         for t in transaction_hist:
             print(t)
+
+        print("\n--------------------------\n")
     
     def __str__(self):
-        return "\nBalance: {}\nPurchased Stocks: {}\nPurchased Mutual Funds: {}\n".format(self.cash, self.stocks, self.mutual_funds)
+        return "\nBalance: {}\nPurchased Stocks: {}\nPurchased Mutual Funds: {}\n".format("{:.2f}".format(self.cash), self.stocks, self.mutual_funds)
 
     def __repr__(self):
         return self.__str__()
