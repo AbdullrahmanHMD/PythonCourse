1fa6e19e7d0d882af56f2c0d9def664902d7db16
AbdullrahmanHMD <manofsteel272@yahoo.com>
2021-04-27 19:00:33 +0300

    Add and implement credibleIntervals function

--- Homework 02/LinearRegression.py
+++ Homework 02/LinearRegression.py
@@ -5,6 +5,9 @@ import datetime
 import os
 import matplotlib.pyplot as plt
 
+from scipy.stats import sem
+import statistics
+
 file_name = "data.csv"
 path = os.path.abspath(os.getcwd()) + '\\' + file_name
 data = pd.read_csv(path).to_numpy()
@@ -27,12 +30,46 @@ def isArrayNumeric(array):
             indexes.append(i)
     return indexes
 
+
+def credibleIntervals(X, Y, y_pred, confidence_level, xshape, yshape):
+    n, k = X.shape
+    low_interval = []
+    high_interval = []
+
+    means = []
+    standard_errors = []
+
+    i = 0
+    variance = 0
+    for sublist in y_pred.reshape(xshape, yshape):
+        samples = []
+        for point in sublist:
+            error = np.subtract(Y.reshape(xshape, yshape)[i], sublist)
+            variance = np.dot(error.T, error) / (yshape - 1 - k)
+            samples.append(point)
+
+        i += 1
+        standard_error = confidence_level * math.sqrt(variance / 5)
+        mean = np.mean(samples)
+
+        low = mean - standard_error 
+        high = mean + standard_error
+
+        low_interval.append(low)
+        high_interval.append(high)
+
+        means.append(mean)
+        standard_errors.append(standard_errors)
+    
+    return low_interval, high_interval, means, standard_errors
+    
 # The linear regression function
 # X: An array of the independent variables of the data.
 # Y: An array of the dependent variables of the data.
 def LinearRegression(X, Y):
 
-    # Handling bad inputs.
+    # --Handling bad inputs part--------------------
+    
     # If the first parameter is not an array.
     if not (isinstance(X, list) or isinstance(X, np.ndarray)):
         raise Exception("Invalid input type")
@@ -53,10 +90,6 @@ def LinearRegression(X, Y):
         if len(X) != len(Y):
             raise Exception("Unmatched input lengths")
             return None, None, None, None
-    # If the two inputs are of different types.
-    else:
-        raise Exception("Parameter types mismatched, both parameter should be of the same type!")
-        return None, None, None, None
             
     # Deleting the NaN values from both X and Y.
     nan_indecies = isArrayNumeric(X)
@@ -69,7 +102,8 @@ def LinearRegression(X, Y):
         del X[i]
         del Y[i]
     
-    # The linear regression part.
+    # --The linear regression part--------------------
+
     X = np.array(X).reshape(len(X), 1)
     n, k = X.shape
 
@@ -96,14 +130,61 @@ def LinearRegression(X, Y):
     beta_variance = np.multiply(variance, np.linalg.inv(temp_mat))
 
     # Calculating the credible intervals with 0.95 confidence.
-    standard_errors = np.divide(np.sqrt(beta_variance.diagonal().astype(float)), math.sqrt(n))
-    credible_intervals = np.array([np.subtract(beta_vals, 1.96 * standard_errors), np.add(beta_vals, 1.96 * standard_errors)])
+    confidence_level = 1.96
+    number_of_samples = 20
+    size_of_sample = 5
+    
+    low_interval, high_interval, means, standard_errors = credibleIntervals(X, Y, y_pred, confidence_level, number_of_samples, size_of_sample)
 
-    return y_pred, standard_errors, credible_intervals[0], credible_intervals[1]
+    return y_pred, standard_errors, low_interval, high_interval, means
 
-y_pred, standard_errors, interval_1, interval_2 = LinearRegression(X, Y_truth)
+y_pred, standard_errors, low_interval, high_interval, means = LinearRegression(X, Y_truth)
 
-# Plotting the data.
+# --Plotting the data-----------------------------
+
+# Plotting the points.
 plt.plot(days_numbering, y_pred)
 plt.scatter(days_numbering, rates, color='green')
-# plt.show()
+
+# Plotting the credible intervals
+plt.plot(days_numbering[::5], low_interval, color='r')
+plt.plot(days_numbering[::5], high_interval, color='r')
+
+plt.xlabel('Date / Day number')
+plt.ylabel('USD to TL rate')
+plt.show()
+# # Plotting the points.
+plt.plot(days_numbering, y_pred)
+plt.scatter(days_numbering, rates, color='green')
+
+# # Plotting the credible intervals
+
+plt.errorbar(x = days_numbering[::5], y=np.array(means), yerr=np.array(standard_errors), color='blue')
+plt.xlabel('Date / Day number')
+plt.ylabel('USD to TL rate')
+plt.show()
+
+
+
+
+
+
+
+
+    # standard_errors = []
+    # j = 0
+    # for i in y_pred.reshape(20, 5):
+    #     samples = []
+    #     var = 0
+    #     for point in y_pred.reshape(20, 5)[j]:
+    #         error = np.subtract(Y.reshape(20, 5)[j], y_pred.reshape(20, 5)[j])
+    #         var = np.dot(error.T, error) / (5 - 1 - k)
+    #         samples.append(point)
+    #     j += 1
+    #     se =  confidence_level * math.sqrt(var / 5)
+    #     mean = np.mean(samples)
+    #     low = mean - se 
+    #     high = mean + se
+
+    #     l_interval.append(low)
+    #     h_interval.append(high)
